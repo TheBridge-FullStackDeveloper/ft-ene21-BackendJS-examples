f1f2b0b9551b9238dae1cd1a744c860f960351b1
thebridge-tech <david.carvajal@thebridgeschool.es>
2021-03-15 12:54:42 +0100

    CRUD examples

--- expressServer.js
+++ expressServer.js
@@ -1,10 +1,23 @@
 // 1) ------ Importar dependencias ------
 const express = require('express');
 const bodyParser = require('body-parser');
+const firebase = require('firebase');
 
 // 2) ------ Configuración inicial ------
 const server = express();
-const listenPort = 8080;
+const listenPort = process.argv[2] || process.env.PORT || 8080;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxDds9V5X4dW-JkTyUMclnZObEPP4313Y",
+  authDomain: "urban-monsters.firebaseapp.com",
+  databaseURL: "https://urban-monsters-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "urban-monsters",
+  storageBucket: "urban-monsters.appspot.com",
+  messagingSenderId: "316615256952",
+  appId: "1:316615256952:web:7b3cc829cad09b9958f68d",
+};
+
+let last = 0;
 
 // Folder with my frontend app
 const staticFilesPath = express.static(__dirname + '/public');
@@ -22,28 +35,70 @@ server.get('/loadImage', (req, res) => {
   res.send({ src: 'img/anchovieFace.jpg' });
 });
 
-const monsters = [{
-  name: "a"
-},
-{
-  name: "b"
-}];
-
+// GET MONSTER
 server.get('/monsters', (req, res) => {
   console.log(req.body); // {} (get no tiene body)
-
-  res.send(JSON.stringify(monsters));
-});
-
-server.post('/monsters', (req, res) => {
-  let data = req.body; // { ... } (post va con datos en body)
   
-  monsters.push({ name: data.monsterName });
+  firebase.database().ref('monsters/').once('value')
+    .then(data => {
+      const responseData = {
+        id: 0, // TODO: Or whatever,
+        monsters: data.val()
+      };
 
-  res.send();
+      res.send(JSON.stringify(responseData));
+    })
+    .catch(error => {
+      res.send(JSON.stringify({ msgError: "No hay datos" }));
+    });
 });
 
-// ...
-server.listen(listenPort,
-  () => console.log(`Server started listening on ${listenPort}`)
-);
+// POST MONSTER (w/ promises)
+server.post('/monsters', (req, res) => {
+  
+  firebase.database().ref('monsters/' + last).update({
+    name: req.body.name
+  })
+  .then(data => {
+    res.send(JSON.stringify({ result: "Ok" }))
+    last++;
+  })
+  .catch(error => {
+    res.send(JSON.stringify({ msgError: "No se puede escribir" }));
+  });
+});
+
+// PUT MONSTER (w/ async/await)
+server.put('/monsters', async (req, res) => {
+  let body = req.body;
+  let params = req.query; // Query strings example
+
+  // Cambiar el nombre de aquel elemento referenciado por el myId
+  // ... Validations ...
+  await firebase.database().ref('monsters/' + params.myId).update({
+    name: body.name
+  })
+
+  res.send("Todo bien");
+});
+
+// DELETE MONSTER (w/ async/await)
+server.delete('/monsters/:myId', async (req, res) => {
+  let params = req.params; // Query params example
+  
+  // Borrrar aquel elemento referenciado por el myId
+  // ... Validations ...
+  await firebase.database().ref('monsters/' + params.myId).remove()
+
+  res.send("Todo bien");
+});
+
+async function init() {
+  await firebase.initializeApp(firebaseConfig);
+
+  server.listen(listenPort,
+    () => console.log(`Server started listening on ${listenPort}`)
+  );
+}
+
+init();
--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,88 @@ document
           .src = data.src
       })
   });
+
+// ----------------- CRUD Operations -----------------
+
+let myId;
+
+// GET MONSTER
+document
+  .querySelector("#monsterMasterBtn")
+  .addEventListener("click", () => {
+    fetch("/monsters")
+      .then(response => response.json())
+      .then(data => {
+        if( data.msgError !== undefined ) {
+          // Pinto el error al usuario
+          // ...
+        }
+        else {
+          //myId = data.id...
+          
+          data.map(monster => {
+            // Pintar el maestro de monstruos
+            // ...
+          })
+        }
+      })
+  });
+
+// POST MONSTER
+document
+  .querySelector("#submitBtn")
+  .addEventListener("click", () => {
+
+    const user = {
+      name: document.querySelector("#monsterName").value
+    };
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch("/monsters", options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.result);
+      })
+  });
+
+// PUT MONSTER
+document
+  .querySelector("#submitUpdateBtn")
+  .addEventListener("click", () => {
+
+    const user = {
+      name: document.querySelector("#monsterName").value
+    };
+
+    const options = {
+      method: 'PUT',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch("/monsters?myId=" + myId, options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.result);
+      })
+  });
+
+// DELETE MONSTER
+document
+  .querySelector("#submitDeleteBtn")
+  .addEventListener("click", () => {
+    fetch("/monsters/" + myId, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.result);
+      })
+  });
